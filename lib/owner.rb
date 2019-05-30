eff49e4cff5f3e3803ef101b975e7e50b85a65d9
Alethea Cary <aletheacary@gmail.com>
2019-05-30 23:50:29 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -37,11 +37,10 @@ class Owner
   
   def buy_cat (name)
     @pets[:cats] << Cat.new(name)
-    
   end
   
-  def buy_dog
-    
+  def buy_dog (name)
+    @pets[:dogs] << Dog.new(name)
   end
   
   
