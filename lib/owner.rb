18cc22e250579e6c9f6add98b2f4645f0d7f63fc
Alethea Cary <aletheacary@gmail.com>
2019-05-30 23:40:11 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,34 @@
+require "pry"
+
 class Owner
-  # code goes here
+  attr_accessor :pets, :name
+  attr_reader :species
+  @@all = []
+  
+  def initialize(species)
+    @name = name
+    @species = species
+    @pets = pets
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.length
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
+  def say_species
+    "I am a #{@species}."
+  end
+  
+  def name
+    @name = name
+  end
 end
